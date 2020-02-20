f8e1eab21dae8e2df4c241c4aafa318464f11a2c
Aidan Middleton <aidanemiddleton@gmail.com>
2020-02-20 23:33:53 +0000

    worked on registration page, added login page

--- express_server.js
+++ express_server.js
@@ -2,7 +2,10 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 const bodyParser = require("body-parser");
+const cookieParser = require('cookie-parser');
+
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser());
 
 app.set('view engine', 'ejs');
 
@@ -16,24 +19,34 @@ function generateRandomString() {
   return results;
 };
 
+function emailExists(email) {
+  for (let userID in users) {
+    if (users[userID].email === email) {
+      return true
+    }
+  }
+  return false;
+}
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
-// const users = { 
-//   "userRandomID": {
-//     id: "userRandomID", 
-//     email: "user@example.com", 
-//     password: "purple-monkey-dinosaur"
-//   },
-//  "user2RandomID": {
-//     id: "user2RandomID", 
-//     email: "user2@example.com", 
-//     password: "dishwasher-funk"
-//   }
-// }
+const users = { 
+  "9xqii7": {
+    id: "9xqii7", 
+    email: "bigcheddar69@killme.com", 
+    password: "pass"
+  },
+ "9d0kj3": {
+    id: "9d0kj3", 
+    email: "user2@example.com", 
+    password: "dishwasher-funk"
+  }
+}
 
+//GET REQUESTS -----------------------------------
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -64,9 +77,40 @@ app.get("/u/:shortURL", (req, res) => {
   res.redirect(longURL);
 });
 
-// app.get("/register", (req, res) => {
-//   res.render("users_new")
-// })
+app.get("/register", (req, res) => {
+  res.render("users_new")
+})
+
+//POST REQUESTS ---------------------------------
+
+app.post("/register", (req, res) => {
+  console.log(req.body);
+  const email = req.body.email;
+  if (!email || !req.body.password) {
+    res.status(400).send("You didn't enter anything, moron")
+  }
+  if (emailExists(email)) {
+    res.status(400).send("There is already an account linked to that e-mail address.");
+    res.redirect('/register');
+  }
+  console.log(users);
+  let newID = generateRandomString();
+  let newUser = {
+    id: newID,
+    email: email,
+    password: req.body.password,
+  };
+  users[newID] = newUser
+  res.cookie("user_id", newID)
+  console.log(users);
+  res.redirect('/urls');
+});
+
+app.post("/login", (req, res) => {
+  console.log('req.body = ', req.body)
+
+  res.redirect('/urls');
+})
 
 app.post("/urls/:shortURL/delete", (req, res) => {
   delete urlDatabase[req.params.shortURL];
